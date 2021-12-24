af95373d97e03174d215edca6e1fab68b0195817
이진영 <jylee@wbsoft.kr>
2021-12-24 16:49:32 +0900

    init

---
+++ blueMarble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  blueMarble
+//
+//  Created by 윌비소프트 on 2021/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
